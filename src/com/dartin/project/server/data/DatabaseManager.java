95c460498507672456d76f7c88c683a29a2c69cf
tupseny <marsik98@gmail.com>
2017-06-30 01:47:41 +0300

    create Table -> Database manager

--- src/com/dartin/project/server/data/DatabaseManager.java
+++ src/com/dartin/project/server/data/DatabaseManager.java
@@ -2,6 +2,10 @@ package com.dartin.project.server.data;
 
 import com.sun.rowset.CachedRowSetImpl;
 
+import javax.jws.Oneway;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.net.DatagramPacket;
 import java.sql.*;
 
 public class DatabaseManager {
@@ -90,5 +94,36 @@ public class DatabaseManager {
 			return 0;
 		}
 	}
+
+	public static void createTable(Object o){
+        String tableName = o.getClass().getSimpleName().toLowerCase();
+        DatabaseManager dm = DatabaseManager.getInstance();
+        StringBuilder str = new StringBuilder();
+        Method[] methods = o.getClass().getMethods();
+
+        str.append("begin;");
+        str.append("CREATE TABLE if NOT EXISTS ");
+        str.append(tableName);
+        str.append("();");
+        for (Method m :
+                methods) {
+            String methodName = m.getName();
+            if (m.getReturnType() == String.class &&
+                    methodName.startsWith("get")){
+                str.append("alter table ");
+                str.append(tableName);
+                str.append(" add ");
+                str.append(methodName.substring(3).toLowerCase());
+                str.append(" text;");
+            }
+        }
+        str.append("commit;");
+
+        try {
+            dm.execUpdate(str.toString());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
